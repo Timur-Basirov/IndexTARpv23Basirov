07607643355942272d6b2898a4a4827a6ec780a4
opilane <opilane@HP-CZC2349HV0>
2024-01-18 12:31:44 +0200

    Add project files.

---
+++ IndexTARpv23Basirov.py
@@ -0,0 +1,24 @@
+print("Tere! Olen sinu uus sõber - Python!")
+nimi=input("Mis on sinu nimi?")
+print(nimi+", oi kui ilus nimi!")
+index=(int(input(nimi+"! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")))
+if index==1:
+    pikkus=float(input("Pikkus on: "))
+    mass=int(input("Mass on: "))
+    indeks=mass/((0.01*pikkus)**2)
+    print(nimi+"! Sinu keha index on:", indeks)
+    if indeks<16:
+        answer="Tervisele ohtlik alakaal"
+    elif indeks<=16:
+        answer="Alakaal"
+    elif indeks<=19:
+        answer="Normaalkaal"
+    elif indeks<=25:
+        answer="Ülekaal"
+    elif indeks<=30:
+        answer="Rasvumine"
+    elif indeks<=35:
+        answer="Tugev rasvumine"
+    elif indeks<=40:
+        answer="Tervisele ohtlik rasvumine"
+    print(answer)
